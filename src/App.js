f4c762b0e80d7468e333cf4414a34be2443a264d
Cristopher D. Chavez <daniel@dsynapse.com>
2021-11-17 21:23:39 -0600

    Clase 10 | Using the useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-  let name = 'mario'
+  const [name, setName] = useState('mario')
 
   const handleClick = () => {
-    name = 'luigi'
+    setName('luigi')
     console.log(name)
   }
  
