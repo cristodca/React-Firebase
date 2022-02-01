58b8b7d967a7f812d519d83dc2293c1e53d00307
@cristodca (Cristopher D. Chavez) <daniel@dsynapse.com>
2022-01-31 21:12:06 -0600

    Clase 41 | The onChange Event

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,17 +1,30 @@
+import { useState } from 'react'
 import './NewEventForm.css'
 
 const NewEventForm = () => {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+
+  // const handleChange = (e) => {
+  //   setTitle(e.target.value)
+  // }
+
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title:</span>
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" 
+          onChange={e => setTitle(e.target.value)} 
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" name="date" id="date" />
+        <input type="date" name="date" id="date" 
+          onChange={e => setDate(e.target.value)} 
+        />
       </label>
       <button type="submit">Submit</button>
+      <p>title - {title}, date - {date}</p>
     </form>
   );
 }
