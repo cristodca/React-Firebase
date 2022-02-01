c92ab30aaac1931c94dd7b64ed6f04e9177d6b0c
@cristodca (Cristopher D. Chavez) <daniel@dsynapse.com>
2022-01-31 21:19:30 -0600

    Clase 42 | Controlled Inputs

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -9,22 +9,30 @@ const NewEventForm = () => {
   //   setTitle(e.target.value)
   // }
 
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
+
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title:</span>
-        <input type="text" name="title" id="title" 
+        <input type="text" name="title" id="title"
+          value={title} 
           onChange={e => setTitle(e.target.value)} 
         />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" name="date" id="date" 
+        <input type="date" name="date" id="date"
+          value={date} 
           onChange={e => setDate(e.target.value)} 
         />
       </label>
       <button type="submit">Submit</button>
       <p>title - {title}, date - {date}</p>
+      <p onClick={resetForm}>reset the form</p>
     </form>
   );
 }
